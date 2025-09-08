c1d6fcb5cc1cb325e6a78af752ae92c965ddcb2a
Romina De León <romideleon@gmail.com>
2025-09-08 12:49:19 -0300

    Update install.R

--- install.R
+++ install.R
@@ -1,9 +1,5 @@
 install.packages(c(
-  "tidyverse", "RColorBrewer", "paletteer", "datos", "gapminder", "tradestatistics", "highcharter",
-  "treemapify", "ggplot2", "rio", "echarts4r", "plotly", "tidytext", "dplyr", "tidyselect",
-  "purrr", "readr", "stringr", "webshot", "htmlwidgets", "pandoc", "forcats",
-"cowplot", "ggrepel", "sf", "rnaturalearth", "countrycode", "scico", "Cairo",
-"colorspace", "Polychrome", "MetBrewer", "ggsci", "zoo"
+  "tidyverse", "tidytext", "dplyr", "tidyselect"
 ))
 
 # Registrar IRkernel para Jupyter
