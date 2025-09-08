04ae8582e362cad8591b668cd3c88229424ed04d
ROmina De León <romideleon@gmail.com>
2025-09-08 12:41:50 -0300

    Configurar entorno R para MyBinder

---
+++ install.R
@@ -0,0 +1,10 @@
+install.packages(c(
+  "tidyverse", "RColorBrewer", "paletteer", "datos", "gapminder", "tradestatistics", "highcharter",
+  "treemapify", "ggplot2", "rio", "echarts4r", "plotly", "tidytext", "dplyr", "tidyselect",
+  "purrr", "readr", "stringr", "webshot", "htmlwidgets", "pandoc", "forcats",
+"cowplot", "ggrepel", "sf", "rnaturalearth", "countrycode", "scico", "Cairo",
+"colorspace", "Polychrome", "MetBrewer", "ggsci", "zoo"
+))
+
+# Registrar IRkernel para Jupyter
+IRkernel::installspec(user = FALSE)
